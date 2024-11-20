d0afd658b8e2f9e0999c49444d3e4a37e63ed270
Muhammad Alif Tajudin <alif@CrazyMans-MacBook-Pro-1066.local>
2024-11-20 08:49:14 +0800

    commit initials

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 echo "BUILD START"
 pip install --upgrade pip
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
-python3.9 manage.py makemigrations
-python3.9 manage.py migrate
+python3.12 -m pip install -r requirements.txt
+python3.12 manage.py collectstatic --noinput --clear
+python3.12 manage.py makemigrations
+python3.12 manage.py migrate
 echo "BUILD END"
